f685baf39db758cfd58169de063d8852b95b95ed
TTChungC <49162615+TTChungC@users.noreply.github.com>
2024-11-08 02:32:49 +0800

    Update selectionp.py

--- selectionp.py
+++ selectionp.py
@@ -5,7 +5,6 @@ from typing import Optional
 from collections import OrderedDict
 from safetensors.torch import load_file
 import os
-import logging
 
 class SelectionP(LlamaForCausalLM): 
     def __init__(self, config): 
@@ -26,15 +25,6 @@ class SelectionP(LlamaForCausalLM):
         return output
 
 def load(checkpoint):
-    original_log_level = logging.getLogger().getEffectiveLevel()
-    logging.disable(logging.WARNING)
     model = SelectionP.from_pretrained(checkpoint)
     tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-    save_dict = load_file(os.path.join(checkpoint, "adapter_model.safetensors"))
-    save_dict['base_model.model.token_weights.weight'], save_dict['base_model.model.token_weights.bias']
-    new_state_dict = OrderedDict()
-    new_state_dict['token_weights.original_module.weight'] = save_dict['base_model.model.token_weights.weight']
-    new_state_dict['token_weights.original_module.bias'] = save_dict['base_model.model.token_weights.bias']
-    model.load_state_dict(new_state_dict, strict=False)
-    logging.disable(original_log_level)
     return model, tokenizer
